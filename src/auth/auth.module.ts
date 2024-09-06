ba39a9a0cace785b851523cf859e67dda81ff817
Ruslan <ssakoks@mail.ru>
2024-09-06 12:21:56 +0500

    add auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from 'src/config/jwt.config'
+import { UserService } from 'src/user/user.service'
+import { JwtStrategy } from './strategies/jwt.strategy'
+
+@Module({
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+	controllers: [AuthController],
+	providers: [AuthService, UserService, JwtStrategy]
+})
+export class AuthModule {}
