3bf8ca7aa0f9a600972869e1f7fa451543fdc6ca
Ruslan <ssakoks@mail.ru>
2024-09-06 12:48:55 +0500

    add store

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from './prisma/prisma.module'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule]
+	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule, StoreModule]
 })
 export class AppModule {}
